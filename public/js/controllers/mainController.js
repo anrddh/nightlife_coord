b4799b2e0335a8eb26d59447a6f446b45b2dabfd
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-08-18 20:09:41 +0800

    Added API for getting data from Yelp

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,12 +1,34 @@
 Nightlife.controller("mainController", function($scope, $http) {
+    function randomString(length, chars) {
+        var result = '';
+        for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+        return result;
+    }
+
     $scope.getBusiness = function() {
-        $http.get({method: 'JSONP', url: "http://api.yelp.com/v2/search?term=bar&location=" + $scope.city})
-            .success(function(venues) {
-                console.log(venues);
+        var method = 'GET';
+        var url = 'http://api.yelp.com/v2/search';
+        var params = {
+            callback: 'angular.callbacks._0',
+            oauth_consumer_key: '4L2iIoI3GZEltc4HHQvgcA',
+            oauth_token: 'v4nIUB7KbVqPbndC6m4elsCrSQh3dF6s',
+            oauth_signature_method: 'HMAC-SHA1',
+            oauth_timestamp: new Date().getTime(),
+            oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+            location: $scope.city,
+            term: 'bar'
+        };
+        var consumerSecret = 'G2cmV4F6pUi_3Pu3bCNp3L1Cobg';
+        var tokenSecret = 'FWtyT7tYHEFr6_agupiwqnMPRXo';
+        var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encoded: false});
+        console.log(signature);
+        params['oauth_signature'] = signature;
+        $http.jsonp(url, {params: params})
+            .success(function(venue) {
+                console.log(venue);
             })
             .error(function(err) {
                 console.log("Error: " + err);
             });
-        console.log($scope.city);
     };
 });
